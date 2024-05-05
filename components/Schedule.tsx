764c18a68b1d6c79ce88fb3a2d08351def1a0728
Kenny Cartman <lyuholu@gmail.com>
2024-05-05 13:29:48 +0800

    feat: add weekdays

--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -1,3 +1,14 @@
 export default function Schedule() {
-  return <div className='flex h-full w-full'></div>;
+  return (
+    <section className='relative flex h-auto w-full overflow-auto'>
+      <section className='fixed left-0 top-[5vh] flex h-6 w-full justify-around'>
+        {[' ', 'S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day) => (
+          <div className='flex h-8 w-[12.5%] items-center justify-center'>
+            {day}
+          </div>
+        ))}
+      </section>
+      <section></section>
+    </section>
+  );
 }
