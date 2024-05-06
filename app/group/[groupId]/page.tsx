e75b773491a6b78e510201bbea3f82a7e40fa525
Kenny Cartman <lyuholu@gmail.com>
2024-05-06 20:11:14 +0800

    feat: allow users to log in

--- app/group/[groupId]/page.tsx
+++ app/group/[groupId]/page.tsx
@@ -1,13 +1,86 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import Schedule from '@/components/Schedule';
 
+const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
+const SUPABASE_URL = 'https://dynrtinrvrbbilkazcei.supabase.co';
+const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
+
+interface User {
+  email: string;
+  avatarUrl: string;
+}
+
 export default function Page({ params }: { params: { groupId: string } }) {
+  const [user, setUser] = useState<User | null>(null);
+
+  async function handleLogin() {
+    const { error } = await supabase.auth.signInWithOAuth({
+      provider: 'google',
+      options: {
+        redirectTo: `http://localhost:3000/group/${params.groupId}`,
+      },
+    });
+
+    if (error) {
+      console.error('Login Error: ', error);
+      alert('Login Error');
+    }
+  }
+
+  async function handleLogout() {
+    const { error } = await supabase.auth.signOut();
+
+    if (error) {
+      console.error('Logout Error: ', error);
+      alert('Logout Error');
+    } else {
+      setUser(null);
+    }
+  }
+
+  useEffect(() => {
+    async function setUp() {
+      try {
+        const {
+          data: { user },
+        } = await supabase.auth.getUser();
+
+        if (user) {
+          setUser({
+            email: user.email ?? '',
+            avatarUrl: user.user_metadata.avatar_url,
+          });
+        }
+      } catch (error) {
+        console.error('Set Up Error: ', error);
+        alert('Set Up Error');
+      }
+    }
+
+    setUp();
+  }, []);
+
   return (
     <>
       <main className='text-white'>
-        <section className='fixed left-0 top-0 z-10 flex h-12 w-screen items-center bg-zinc-800 px-4 font-bold'>
-          <div className='cursor-pointer text-emerald-500'>
-            Choose a Schedule
-          </div>
+        <section className='fixed left-0 top-0 z-10 flex h-12 w-screen items-center justify-end bg-zinc-800 px-4 font-bold'>
+          {user ? (
+            <img
+              src={user.avatarUrl}
+              className='h-8 w-8 cursor-pointer rounded-full'
+              onClick={handleLogout}
+            />
+          ) : (
+            <button
+              className='cursor-pointer text-emerald-500'
+              onClick={handleLogin}
+            >
+              Log In
+            </button>
+          )}
         </section>
         <Schedule />
       </main>
--- components/Schedule.tsx
+++ components/Schedule.tsx
@@ -2,17 +2,23 @@ export default function Schedule() {
   return (
     <section>
       <section className='fixed left-0 top-12 z-10 flex h-12 w-full justify-around bg-zinc-800'>
-        {[' ', 'S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day) => (
-          <span className='flex h-full w-[12.5%] items-center justify-center font-bold'>
+        {[' ', 'S', 'M', 'T', 'W', 'T', 'F', 'S'].map((day, i) => (
+          <span
+            key={i}
+            className='flex h-full w-[12.5%] items-center justify-center font-bold'
+          >
             {day}
           </span>
         ))}
       </section>
       <section className='mt-28'>
         {new Array(24).fill(null).map((row, i) => (
-          <div className='flex h-16 w-screen'>
+          <div key={i} className='flex h-16 w-screen'>
             {new Array(8).fill(null).map((col, j) => (
-              <div className='relative h-full w-[12.8%] cursor-pointer border-r-2 border-t-2 first:border-0 last:border-r-0'>
+              <div
+                key={j}
+                className='relative h-full w-[12.8%] cursor-pointer border-r-2 border-t-2 first:cursor-default first:border-0 last:border-r-0'
+              >
                 {j === 0 && (
                   <span className='left-0flex absolute -top-2 flex w-full items-center justify-center text-xs'>
                     {i.toString().padStart(2, '0')}:00
