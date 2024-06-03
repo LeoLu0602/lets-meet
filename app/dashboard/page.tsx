30c6d1cf33add1f3e2b8aa6972341795890a923a
Kenny Cartman <lyuholu@gmail.com>
2024-06-03 15:22:24 +0800

    feat: add groups to dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,10 +4,17 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { User } from '@supabase/supabase-js';
 import { UserInfo } from '@/app/interfacesAndTypes';
-import { handleLogin, handleLogout, retrieveUser } from '@/app/lib/utilities';
+import {
+  handleLogin,
+  handleLogout,
+  retrieveUser,
+  getGroups,
+} from '@/app/lib/utilities';
+import { group } from 'console';
 
 export default function Page() {
   const [user, setUser] = useState<UserInfo | null>(null);
+  const [groups, setGroups] = useState<{ name: string; id: string }[]>([]);
 
   const router = useRouter();
 
@@ -25,14 +32,18 @@ export default function Page() {
         email: user.user_metadata.email,
         avatarUrl: user.user_metadata.avatar_url,
       });
+
+      const groups: { name: string; id: string }[] = await getGroups(user.id);
+
+      setGroups(groups);
     }
   }
 
   return (
     <>
-      <main>
-        <section className='flex h-12 w-screen items-center justify-between px-4'>
-          <h2 className='text-xl font-bold text-white'>Dashboard</h2>
+      <main className='absolute left-0 top-0 w-screen p-4'>
+        <section className='flex h-12 w-full items-center justify-between'>
+          <h2 className='text-2xl font-bold text-white'>Dashboard</h2>
           {user ? (
             <button
               className='font-bold text-rose-500'
@@ -55,22 +66,39 @@ export default function Page() {
           )}
         </section>
 
-        <a className='px-4 font-bold text-sky-500' href='/'>
+        <a className='my-4 block font-bold text-sky-500' href='/'>
           New Group
         </a>
 
         {user && (
-          <section className='w-screen p-4 font-bold text-white'>
-            <section className='flex items-center gap-2'>
-              <img src={user.avatarUrl} className='h-8 w-8 rounded-full' />
-              <span className='overflow-hidden text-ellipsis whitespace-nowrap text-2xl'>
-                {user.username}
-              </span>
+          <>
+            <section className='font-bold text-white'>
+              <section className='flex items-center gap-2'>
+                <img src={user.avatarUrl} className='h-8 w-8 rounded-full' />
+                <span className='overflow-hidden text-ellipsis whitespace-nowrap text-2xl'>
+                  {user.username}
+                </span>
+              </section>
+              <section className='mt-2 overflow-hidden text-ellipsis whitespace-nowrap'>
+                {user.email}
+              </section>
             </section>
-            <section className='mt-2 overflow-hidden text-ellipsis whitespace-nowrap'>
-              {user.email}
-            </section>
-          </section>
+
+            <h2 className='my-8 text-2xl font-bold text-emerald-500'>
+              Your Groups
+            </h2>
+
+            <ul className='font-bold text-white'>
+              {groups.map((group) => (
+                <li
+                  key={group.id}
+                  className='mb-4 cursor-pointer text-sky-500 last:mb-0'
+                >
+                  <a href={`/group/${group.id}`}>{group.name}</a>
+                </li>
+              ))}
+            </ul>
+          </>
         )}
       </main>
     </>
--- app/lib/utilities.tsx
+++ app/lib/utilities.tsx
@@ -1,4 +1,9 @@
-import { AuthError, User, createClient } from '@supabase/supabase-js';
+import {
+  AuthError,
+  PostgrestError,
+  User,
+  createClient,
+} from '@supabase/supabase-js';
 
 const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL ?? '';
 const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ?? '';
@@ -42,4 +47,40 @@ export async function retrieveUser(): Promise<User | null> {
 
     return null;
   }
+}
+
+export async function getGroups(
+  userId: string
+): Promise<{ name: string; id: string }[]> {
+  const groupIds = await getGroupIds(userId);
+
+  const { data, error }: { data: any[] | null; error: PostgrestError | null } =
+    await supabase.from('group').select('*').in('id', groupIds);
+
+  if (error) {
+    console.error('Get Groups Error: ', error);
+    alert('Get Groups Error');
+
+    return [];
+  }
+
+  return (
+    data?.map((group) => {
+      return { name: group.name, id: group.id };
+    }) ?? []
+  );
+}
+
+export async function getGroupIds(userId: string): Promise<string[]> {
+  const { data, error }: { data: any[] | null; error: PostgrestError | null } =
+    await supabase.from('group_user').select('*').eq('user_id', userId);
+
+  if (error) {
+    console.error('Get Group IDs Error: ', error);
+    alert('Get Group IDs Error');
+
+    return [];
+  }
+
+  return data?.map((groupUser) => groupUser.group_id) ?? [];
 }
